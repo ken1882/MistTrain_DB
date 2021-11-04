76680a4a841fe96467a8ba9afea658d5da009266
ken1882 <qj6w94g0q@gmail.com>
2021-11-04 20:52:06 +0800

    fix production setting

--- _G.py
+++ _G.py
@@ -32,7 +32,7 @@ FlagRunning = True
 FlagPaused  = False
 FlagWorking = False
 FlagProcessingUserInput = False
-FlagUseCloudData = False
+FlagUseCloudData = True
 
 MSG_PIPE_CONT   = '\x00\x50\x00CONTINUE\x00'
 MSG_PIPE_STOP   = "\x00\x50\x00STOP\x00"
