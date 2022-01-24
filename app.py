7b3afc7b30f87933d641e12628b939c8f1dff00b
ken1882 <qj6w94g0q@gmail.com>
2022-01-24 17:09:32 +0800

    fix derpy data not saving

--- app.py
+++ app.py
@@ -131,7 +131,8 @@ def get_story_content(id):
 def setup():
   derpy.init()
   story.init()
-  return
+  if not _G.FlagUseCloudData:
+    return
   if not game.is_connected():
     res = game.reauth_game()
     if res == _G.ERRNO_MAINTENANCE:
