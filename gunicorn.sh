4aca08c9bb4c14e174511e471a78686e56cc8719
compeador <qj6w94g0q@gmail.com>
2022-09-03 20:45:27 +0000

    update gunicorn

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,1 +1,1 @@
-gunicorn --workers 2 --bind unix:gunicorn.sock --capture-output --log-level debug -m 007 app:app
+sudo sg www-data -c '/home/compeador/miniconda3/bin/gunicorn --workers 2 --bind unix:gunicorn.sock --capture-output --log-level debug -m 007 app:app'
