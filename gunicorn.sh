bae843c64941b5d46fb25fdb46eb9b94712d2fb6
Compeador <qj6w94g0q@gmail.com>
2022-11-20 12:00:21 +0800

    update latest

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,1 +1,1 @@
-sudo sg www-data -c '/home/compeador/miniconda3/bin/gunicorn --workers 2 --bind=0.0.0.0:5000 --capture-output --log-level debug -m 007 app:app'
+bash -c '/home/compeador/miniconda3/bin/gunicorn --workers 2 --bind=0.0.0.0:5000 --capture-output --log-level debug -m 007 app:app'
