5b42f69f5a38a78cfa759f2f3feaaef5f9a1361c
Compeador <qj6w94g0q@gmail.com>
2025-02-12 11:31:50 +0800

    feat: add main story vol.5

--- static/js/story_main.js
+++ static/js/story_main.js
@@ -24,6 +24,7 @@ const __MapURI = {
   '2': 'https://assets4.mist-train-girls.com/production-client-web/81/res/raw-assets/61/615148e8-5947-412a-8b5c-eeeb12f5593a.jpg',
   '3': 'https://assets4.mist-train-girls.com/production-client-web/81/res/raw-assets/61/615148e8-5947-412a-8b5c-eeeb12f5593a.jpg',
   '4': 'https://assets4.mist-train-girls.com/production-client-web/83/res/raw-assets/63/63d4f1ac-f1f7-476f-b744-f8f38902a445.jpg',
+  '5': 'https://assets4.mist-train-girls.com/production-client-web/83/res/raw-assets/63/63d4f1ac-f1f7-476f-b744-f8f38902a445.jpg',
 }
 
 function init(){
